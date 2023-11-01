785cc46002515879de7f31de6e6c7303adc55b20
ABourdarie <alexandre.bourdarie@gmail.com>
2023-11-01 08:32:03 -0400

    Auto stash before merge of "master" and "origin/master"

---
+++ src/Private/Math/Stalk.cpp
@@ -0,0 +1,22 @@
+﻿#include "Stalk.h"
+
+#include "Vector3D.h"
+
+Stalk::Stalk(GameObject* p1, GameObject* p2, double springConstant, double restLength,double damping, double separationDistance)
+    : Spring(p1, p2, springConstant, restLength, damping), separationDistance(separationDistance) {}
+
+void Stalk::applyForce() {
+    Vector3D displacement = firstGameObject->GetPosition() - secondGameObject->GetPosition();
+    double distance = displacement.Magnitude();
+
+    // Calculate the required displacement to maintain the separation distance
+    double requiredDisplacement = separationDistance - distance;
+    Vector3D correction = displacement.Normalize() * requiredDisplacement;
+
+    // Apply the correction to both particles
+    Vector3D correction1 = correction * 0.5;
+    Vector3D correction2 = correction1.Negate();
+
+    firstGameObject->AddPosition(correction1);
+    secondGameObject->AddPosition(correction2);
+}
---
+++ src/Public/Math/Stalk.h
@@ -0,0 +1,20 @@
+﻿#ifndef STALK_H
+#define STALK_H
+
+#include "Spring.h"
+#include "../../Public/GameObjects/GameObject.h"
+
+class Stalk : public Spring {
+public:
+    Stalk() {};
+    Stalk(GameObject* p1, GameObject* p2, double springConstant, double restLength,double damping, double separationDistance);
+
+    virtual void applyForce() override;
+
+private:
+    double separationDistance;
+};
+
+#endif // STALK_H
+
+
